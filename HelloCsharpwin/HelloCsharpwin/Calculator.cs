d1d18d1ce6387067fc35315d1da31d73f649ffc5
Kwon Juyeong <20161527@vision.hoseo.edu>
2024-01-01 17:33:06 +0900

    예외처리

--- HelloCsharpwin/HelloCsharpwin/Calculator.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.cs
@@ -9,6 +9,7 @@ namespace HelloCsharpwin
 
         private void HelloLabel_Click(object sender, EventArgs e)
         {
+     
             int number1 = 1;
             int number2 = 2;
 
@@ -21,15 +22,44 @@ namespace HelloCsharpwin
 
         private void SumNumbers_Click(object sender, EventArgs e)
         {
-            int number1 = Convert.ToInt32(Sum1.Text);
-            int number2 = Convert.ToInt32(Sum2.Text);
+
+            int number1 = 0;
+            int number2 = 0;
+           
+            if (String.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                MessageBox.Show("Sum1에 숫자를 입력해주세요.");
+                Sum1.Focus();
+                return;
+
+            }
+
+            if (int.TryParse(Sum1.Text, out number1) == false)
+            {
+                MessageBox.Show("Sum1에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                Sum1.SelectAll();
+                Sum1.Focus();
+                return;
+
+            }
+            
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("Sum2에 숫자를 입력해주세요.");
+                return;
+            }
+
+            if (int.TryParse(Sum2.Text, out number2) == false)
+            {
+                MessageBox.Show("Sum2에 문자가 들어왔습니다. 숫자를 입력해주세요.");
+                return;
+
+            }
 
             int sum = Add(number1, number2);
-
             SumResult.Text = sum.ToString();
 
         }
-
 
         public int Add(int number1, int number2) {
 
@@ -43,17 +73,12 @@ namespace HelloCsharpwin
             float sum = number1 + number2;
             return sum;
         }
-
-
-
         public int Sub(int number1, int number2)
         {
-
             int sub = number1 - number2;
             return sub;
 
         }
-
 
 
 
