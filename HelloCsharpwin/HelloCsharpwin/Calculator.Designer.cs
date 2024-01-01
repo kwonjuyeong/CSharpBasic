63f2ea50dd391def487cae6a9ca184fb9c22a2d5
Kwon Juyeong <20161527@vision.hoseo.edu>
2024-01-01 15:31:56 +0900

    계산기 만들기

--- HelloCsharpwin/HelloCsharpwin/Calculator.Designer.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             HelloLabel = new Label();
+            Sum2 = new TextBox();
+            Sum1 = new TextBox();
+            SumNumbers = new Button();
+            SumResult = new TextBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // HelloLabel
@@ -41,11 +46,56 @@
             HelloLabel.Text = "여기를 클릭하세요";
             HelloLabel.Click += HelloLabel_Click;
             // 
+            // Sum2
+            // 
+            Sum2.Location = new Point(165, 83);
+            Sum2.Name = "Sum2";
+            Sum2.Size = new Size(100, 23);
+            Sum2.TabIndex = 1;
+            // 
+            // Sum1
+            // 
+            Sum1.Location = new Point(33, 84);
+            Sum1.Name = "Sum1";
+            Sum1.Size = new Size(100, 23);
+            Sum1.TabIndex = 2;
+            // 
+            // SumNumbers
+            // 
+            SumNumbers.Location = new Point(290, 83);
+            SumNumbers.Name = "SumNumbers";
+            SumNumbers.Size = new Size(75, 23);
+            SumNumbers.TabIndex = 3;
+            SumNumbers.Text = "=";
+            SumNumbers.UseVisualStyleBackColor = true;
+            SumNumbers.Click += SumNumbers_Click;
+            // 
+            // SumResult
+            // 
+            SumResult.Location = new Point(391, 83);
+            SumResult.Name = "SumResult";
+            SumResult.Size = new Size(102, 23);
+            SumResult.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(142, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(15, 15);
+            label1.TabIndex = 5;
+            label1.Text = "+";
+            // 
             // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(SumResult);
+            Controls.Add(SumNumbers);
+            Controls.Add(Sum1);
+            Controls.Add(Sum2);
             Controls.Add(HelloLabel);
             Name = "Calculator";
             Text = "Form1";
@@ -56,5 +106,10 @@
         #endregion
 
         private Label HelloLabel;
+        private TextBox Sum2;
+        private TextBox Sum1;
+        private Button SumNumbers;
+        private TextBox SumResult;
+        private Label label1;
     }
 }
--- HelloCsharpwin/HelloCsharpwin/Calculator.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.cs
@@ -9,8 +9,6 @@ namespace HelloCsharpwin
 
         private void HelloLabel_Click(object sender, EventArgs e)
         {
-            HelloLabel.Text = "Hello C#";
-
             int number1 = 1;
             int number2 = 2;
 
@@ -19,6 +17,47 @@ namespace HelloCsharpwin
             HelloLabel.Text = sum.ToString();
 
         }
+
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Sum1.Text);
+            int number2 = Convert.ToInt32(Sum2.Text);
+
+            int sum = Add(number1, number2);
+
+            SumResult.Text = sum.ToString();
+
+        }
+
+
+        public int Add(int number1, int number2) {
+
+            int sum = number1 + number2;
+            return sum;
+
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+
+
+        public int Sub(int number1, int number2)
+        {
+
+            int sub = number1 - number2;
+            return sub;
+
+        }
+
+
+
+
+
 
     }
 }
