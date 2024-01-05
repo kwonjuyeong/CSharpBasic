4c8f8db14eabadb5dff51240454fa33ad104e972
Kwon Juyeong <20161527@vision.hoseo.edu>
2024-01-05 20:54:37 +0900

    사칙연산, 부호변경, 등호, 숫자제거 기능 구현

--- HelloCsharpwin/HelloCsharpwin/Calculator.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.cs
@@ -44,9 +44,9 @@ namespace HelloCsharpwin
 
         }
 
+
         private void OptBtn_Click(object sender, EventArgs e)
         {
-
             if (isNewNum == false)
             {
                 double num = double.Parse(NumScreen.Text);
@@ -56,15 +56,17 @@ namespace HelloCsharpwin
                 else if (Opt == Operators.Sub)
                     Result -= num;
                 else if (Opt == Operators.Multi)
+                {
                     Result *= num;
-                else if (Opt == Operators.Div && num != 0) // 0으로 나누는 것을 방지
+                }
+                else if (Opt == Operators.Div && num != 0)
+                {
                     Result /= num;
+                }
 
                 NumScreen.Text = Result.ToString();
                 isNewNum = true;
-
             }
-
 
             Button optButton = (Button)sender;
 
@@ -72,14 +74,13 @@ namespace HelloCsharpwin
                 Opt = Operators.Add;
             else if (optButton.Text == "-")
                 Opt = Operators.Sub;
-            else if (optButton.Text == "*")
+            else if (optButton.Text == "x")
                 Opt = Operators.Multi;
             else if (optButton.Text == "÷")
                 Opt = Operators.Div;
-
-
         }
 
+        //Clear(전체 지움) 
         private void ClearBtn_Click(object sender, EventArgs e)
         {
             Result = 0;
@@ -89,19 +90,33 @@ namespace HelloCsharpwin
             NumScreen.Text = "0";
         }
 
+        //Back(하나씩 지움)
+        //예외처리 : 글자 길이가 1보다 작으면 0으로 변경
         private void backBtn_Click(object sender, EventArgs e)
         {
-            if (NumScreen.Text.Length > 0)
-            {
+            if (NumScreen.Text.Length > 1)
                 NumScreen.Text = NumScreen.Text.Substring(0, NumScreen.Text.Length - 1);
-            }
+            else
+                NumScreen.Text = "0";
+            
 
         }
 
+        //+/- 음수 양수 변환
         private void switchPM_Click(object sender, EventArgs e)
         {
-            Result *= -1;
-            NumScreen.Text = Result.ToString();
+            if (double.TryParse(NumScreen.Text, out double currentNumber))
+            {
+                currentNumber *= -1;
+                NumScreen.Text = currentNumber.ToString();
+                Result = currentNumber; // 연산 결과인 Result도 부호 변경
+            }
+            else
+            {
+                NumScreen.Text = "0";
+            }
         }
+
+
     }
 }
