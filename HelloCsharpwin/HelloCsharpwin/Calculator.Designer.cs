2e834520816d3755a654cfa860add841fdbe3a6c
Kwon Juyeong <20161527@vision.hoseo.edu>
2024-01-05 17:35:49 +0900

    계산기 UI 완성

--- HelloCsharpwin/HelloCsharpwin/Calculator.Designer.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.Designer.cs
@@ -40,13 +40,19 @@
             Num8 = new Button();
             Num9 = new Button();
             Num0 = new Button();
+            minus = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dot = new Button();
+            switchPM = new Button();
             SuspendLayout();
             // 
             // Num1
             // 
             Num1.BackColor = Color.White;
             Num1.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num1.Location = new Point(12, 302);
+            Num1.Location = new Point(12, 300);
             Num1.Name = "Num1";
             Num1.Size = new Size(85, 55);
             Num1.TabIndex = 0;
@@ -58,7 +64,7 @@
             // 
             Num2.BackColor = Color.White;
             Num2.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num2.Location = new Point(110, 304);
+            Num2.Location = new Point(101, 300);
             Num2.Name = "Num2";
             Num2.Size = new Size(85, 55);
             Num2.TabIndex = 1;
@@ -73,15 +79,15 @@
             NumScreen.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
             NumScreen.Location = new Point(12, 21);
             NumScreen.Name = "NumScreen";
-            NumScreen.Size = new Size(471, 50);
+            NumScreen.Size = new Size(357, 69);
             NumScreen.TabIndex = 2;
             NumScreen.Text = "0";
             NumScreen.TextAlign = ContentAlignment.MiddleRight;
             // 
             // plus
             // 
-            plus.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            plus.Location = new Point(353, 307);
+            plus.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold);
+            plus.Location = new Point(284, 300);
             plus.Name = "plus";
             plus.Size = new Size(85, 55);
             plus.TabIndex = 3;
@@ -93,13 +99,13 @@
             // 
             Num3.BackColor = Color.White;
             Num3.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num3.Location = new Point(206, 307);
+            Num3.Location = new Point(192, 300);
             Num3.Name = "Num3";
             Num3.Size = new Size(85, 55);
             Num3.TabIndex = 4;
             Num3.Text = "3";
             Num3.UseVisualStyleBackColor = false;
-            Num3.Click += button1_Click;
+            Num3.Click += Num3_Click;
             // 
             // Num4
             // 
@@ -111,78 +117,155 @@
             Num4.TabIndex = 5;
             Num4.Text = "4";
             Num4.UseVisualStyleBackColor = false;
+            Num4.Click += Num4_Click;
             // 
             // Num5
             // 
             Num5.BackColor = Color.White;
             Num5.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num5.Location = new Point(110, 241);
+            Num5.Location = new Point(101, 241);
             Num5.Name = "Num5";
             Num5.Size = new Size(85, 55);
             Num5.TabIndex = 6;
             Num5.Text = "5";
             Num5.UseVisualStyleBackColor = false;
+            Num5.Click += Num5_Click;
             // 
             // Num6
             // 
             Num6.BackColor = Color.White;
             Num6.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num6.Location = new Point(206, 241);
+            Num6.Location = new Point(191, 241);
             Num6.Name = "Num6";
             Num6.Size = new Size(85, 55);
             Num6.TabIndex = 7;
             Num6.Text = "6";
             Num6.UseVisualStyleBackColor = false;
+            Num6.Click += Num6_Click;
             // 
             // Num7
             // 
             Num7.BackColor = Color.White;
             Num7.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num7.Location = new Point(12, 170);
+            Num7.Location = new Point(12, 180);
             Num7.Name = "Num7";
             Num7.Size = new Size(85, 55);
             Num7.TabIndex = 8;
             Num7.Text = "7";
             Num7.UseVisualStyleBackColor = false;
+            Num7.Click += Num7_Click;
             // 
             // Num8
             // 
             Num8.BackColor = Color.White;
             Num8.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num8.Location = new Point(110, 170);
+            Num8.Location = new Point(101, 180);
             Num8.Name = "Num8";
             Num8.Size = new Size(85, 55);
             Num8.TabIndex = 9;
             Num8.Text = "8";
             Num8.UseVisualStyleBackColor = false;
+            Num8.Click += Num8_Click;
             // 
             // Num9
             // 
             Num9.BackColor = Color.White;
             Num9.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num9.Location = new Point(206, 170);
+            Num9.Location = new Point(191, 180);
             Num9.Name = "Num9";
             Num9.Size = new Size(85, 55);
             Num9.TabIndex = 10;
             Num9.Text = "9";
             Num9.UseVisualStyleBackColor = false;
+            Num9.Click += Num9_Click;
             // 
             // Num0
             // 
             Num0.BackColor = Color.White;
             Num0.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
-            Num0.Location = new Point(110, 366);
+            Num0.Location = new Point(101, 360);
             Num0.Name = "Num0";
             Num0.Size = new Size(85, 55);
             Num0.TabIndex = 11;
             Num0.Text = "0";
             Num0.UseVisualStyleBackColor = false;
+            Num0.Click += Num0_Click;
+            // 
+            // minus
+            // 
+            minus.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold);
+            minus.Location = new Point(284, 241);
+            minus.Name = "minus";
+            minus.Size = new Size(85, 55);
+            minus.TabIndex = 12;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(284, 360);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 55);
+            button1.TabIndex = 13;
+            button1.Text = "=";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold);
+            button2.Location = new Point(284, 180);
+            button2.Name = "button2";
+            button2.Size = new Size(85, 55);
+            button2.TabIndex = 14;
+            button2.Text = "x";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold);
+            button3.Location = new Point(284, 120);
+            button3.Name = "button3";
+            button3.Size = new Size(85, 55);
+            button3.TabIndex = 15;
+            button3.Text = "%";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dot
+            // 
+            dot.BackColor = Color.White;
+            dot.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            dot.Location = new Point(192, 360);
+            dot.Name = "dot";
+            dot.Size = new Size(85, 55);
+            dot.TabIndex = 16;
+            dot.Text = ".";
+            dot.UseVisualStyleBackColor = false;
+            // 
+            // switchPM
+            // 
+            switchPM.BackColor = Color.White;
+            switchPM.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            switchPM.Location = new Point(12, 359);
+            switchPM.Name = "switchPM";
+            switchPM.Size = new Size(85, 55);
+            switchPM.TabIndex = 17;
+            switchPM.Text = "+/-";
+            switchPM.UseVisualStyleBackColor = false;
             // 
             // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(498, 500);
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(388, 429);
+            Controls.Add(switchPM);
+            Controls.Add(dot);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(minus);
             Controls.Add(Num0);
             Controls.Add(Num9);
             Controls.Add(Num8);
@@ -214,5 +297,11 @@
         private Button Num8;
         private Button Num9;
         private Button Num0;
+        private Button minus;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button dot;
+        private Button switchPM;
     }
 }
--- HelloCsharpwin/HelloCsharpwin/Calculator.cs
+++ HelloCsharpwin/HelloCsharpwin/Calculator.cs
@@ -1,15 +1,25 @@
 namespace HelloCsharpwin
 {
+
+    /*enum = 열거형 데이터 타입 -> 선언을 하고 사용할 수 있다.
+    public enum Season {Spring, Summer, Fall, Winter};
+    Season currentSeason = Season.Spring;
+    */
+
+    public enum Operators {Add, Sub, Multi, Div}
+
     public partial class Calculator : Form
     {
         public int Result = 0;
         public bool isNewNum = true;
+        public Operators Opt = Operators.Add;
 
 
 
         public Calculator()
         {
             InitializeComponent();
+            
         }
 
 
@@ -34,14 +44,56 @@ namespace HelloCsharpwin
 
         private void NumButton1_Click(object sender, EventArgs e)
         {
-
-            SetNum("1");
+            Button numButton = (Button)sender;
+            SetNum(numButton.Text);
         }
 
         private void NumButton2_Click(object sender, EventArgs e)
         {
             SetNum("2");
         }
+
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            SetNum("3");
+        }
+
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            SetNum("4");
+        }
+
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            SetNum("5");
+        }
+
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            SetNum("6");
+
+        }
+
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            SetNum("7");
+        }
+
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            SetNum("8");
+        }
+
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            SetNum("9");
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            SetNum("0");
+        }
+
 
         public void SetNum(string num)
         {
@@ -73,9 +125,8 @@ namespace HelloCsharpwin
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+    
 
-        }
+
     }
 }
