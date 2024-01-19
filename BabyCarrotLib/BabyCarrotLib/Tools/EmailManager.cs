90de6c491091d0763d4642b38372f21be3ca87eb
Kwon Juyeong <20161527@vision.hoseo.edu>
2024-01-19 19:10:17 +0900

    .Net version 이슈 해결 및 EmailSender 라이브러리

---
+++ BabyCarrotLib/BabyCarrotLib/Tools/EmailManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace BabyCarrotLib.Tools
+{
+    public class EmailManager
+    {
+
+        public static void Send(string to, string subject, string contents)
+        {
+            string sender = ConfigurationManager.AppSettings["SMTPSender"];
+
+            string smtpHost = ConfigurationManager.AppSettings["SMTPHost"];
+            int smtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPost"]);
+
+            //Host와 Port번호로 아무나 와서 스팸 메일을 계속해서 보낸다면 블랙 리스트에 올라 해당 서버에서 전송되는 메일이 차단되게 되는 현상을 막기 위함. 
+            string smtpId = ConfigurationManager.AppSettings["SMTPID"]; ;
+            string smtpPwd = ConfigurationManager.AppSettings["SMTPPassword"]; ;
+
+            //Package
+            MailMessage mailMsg = new MailMessage();
+            mailMsg.From = new MailAddress(sender);
+            mailMsg.To.Add(new MailAddress(to));
+            
+            mailMsg.Subject = subject;
+            mailMsg.IsBodyHtml = true;
+            mailMsg.Body = contents;
+            mailMsg.Priority = MailPriority.Normal;
+            //
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Credentials = new NetworkCredential(smtpId, smtpPwd);
+            smtpClient.Host = smtpHost;
+            smtpClient.Port = smtpPort;
+            smtpClient.Send(mailMsg);
+
+
+
+        }
+
+
+    }
+}
--- BabyCarrotLib/BabyCarrotTest/Program.cs
+++ BabyCarrotLib/BabyCarrotTest/Program.cs
@@ -1,27 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using BabyCarrotLib.Tools;
 using BabyCarrotLib.Extensions;
 
-
-/*
-LogManager log = new LogManager(null, "_BabyCarrotText");
-
-log.WriteLine("[Begin Processing]-----");
-
-    for (int index = 0; index < 10; index++)
+namespace BabyCarrotTest
+{
+    internal class Program
     {
-        log.WriteLine("Processing : " + index);
+        static void Main(string[] args)
+        {
 
-        Thread.Sleep(500);
+            /*
+            LogManager log = new LogManager(null, "_BabyCarrotText");
 
-        log.WriteLine("Done : " + index);
+            log.WriteLine("[Begin Processing]-----");
+
+                for (int index = 0; index < 10; index++)
+                {
+                    log.WriteLine("Processing : " + index);
+
+                    Thread.Sleep(500);
+
+                    log.WriteLine("Done : " + index);
+                }
+
+            log.WriteLine("[End Processing]-----");
+            */
+
+            string temp = "12/08/2015 10:10";
+
+            Console.WriteLine("IsNumeric? : " + temp.IsNumeric());
+            Console.WriteLine("IsDateTime? : " + temp.IsDateTime());
+
+
+            EmailManager.Send(ToString, subject, contents);
+
+
+        }
     }
-
-log.WriteLine("[End Processing]-----");
-*/
-
-string temp = "12/08/2015 10:10";
-
-Console.WriteLine("IsNumeric? : " + temp.IsNumeric());
-Console.WriteLine("IsDateTime? : " + temp.IsDateTime());
+}
